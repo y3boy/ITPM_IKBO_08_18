0984aeb8f8ce8ae2bd4252f1b0edb73df5fb474b
Metaraddin <48632825+Metaraddin@users.noreply.github.com>
2021-10-25 10:00:10 +0300

    upd models and crud

--- src/database/crud.py
+++ src/database/crud.py
@@ -1,50 +1,74 @@
-# Реализовать добавление удаление для Client и Walker'a
-# Не забывайте что у нас связаны таблицы сверху с User'ом
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import User, Client, Walker
-from database import SessionLocal
+from src.database.models import User, Client, Walker
+from src.database.database import SessionLocal
 
 
-def client_add(**kwargs):
-    s = SessionLocal()
+def __user_create(username, password, fullname=None, phone=None, email=None):
+    return User(
+        username=username,
+        password=password,
+        fullname=fullname,
+        phone=phone,
+        email=email
+    )
+
+
+def client_create(username, password, fullname=None, phone=None, email=None):
+    session = SessionLocal()
+
     client = Client()
-    s.add(client)
-    s.commit()
-    user = User(
-        firstname=kwargs['firstname'],
-        surname=kwargs['surname'],
-        patronimyc=kwargs['patronimyc'],
-        phone=kwargs['phone'],
-        email=kwargs['email'],
-        password=kwargs['password'],
-        username=kwargs['username'],
-        client_id=client.id,
-    )
-    s.add(user)
-    s.commit()
+    session.add(client)
+    session.commit()
+
+    user = __user_create(username, password, fullname, phone, email)
+    user.client_id = client.id
+    session.add(user)
+    session.commit()
+    return user
 
 
-def walker_add(**kwargs):
-    s = SessionLocal()
+def walker_create(username, password, fullname=None, phone=None, email=None):
+    session = SessionLocal()
+
     walker = Walker()
-    s.add(walker)
-    s.commit()
-    user = User(
-        firstname=kwargs['firstname'],
-        surname=kwargs['surname'],
-        patronimyc=kwargs['patronimyc'],
-        phone=kwargs['phone'],
-        email=kwargs['email'],
-        password=kwargs['password'],
-        username=kwargs['username'],
-        walker_id=walker.id,
-    )
-    s.add(user)
-    s.commit()
+    session.add(walker)
+    session.commit()
+
+    user = __user_create(username, password, fullname, phone, email)
+    user.walker_id = walker.id
+    session.add(user)
+    session.commit()
+    return user
+
+
+def user_read(user_id):
+    session = SessionLocal()
+    return session.query(User).get(user_id)
+
+
+def user_read_by_username(username):
+    session = SessionLocal()
+    return session.query(User).filter(User.username == username).first()
+
+
+def user_update(user_id, password=None, fullname=None, phone=None, email=None):
+    session = SessionLocal()
+    user = session.query(User).get(user_id)
+    if password:
+        user.password = password
+    if fullname:
+        user.fullname = fullname
+    if phone:
+        user.phone = phone
+    if email:
+        user.email = email
+    session.add(user)
+    session.commit()
+    return user
 
 
 def user_delete(user_id):
-    s = SessionLocal()
-    s.delete(s.query(User).filter(User.name == user_id))
-    s.commit()
+    session = SessionLocal()
+    user = session.query(User).get(user_id)
+    session.delete(user)
+    session.commit()
+    return user
--- src/database/models.py
+++ src/database/models.py
@@ -7,13 +7,11 @@ from src.database.database import DataBase
 class User(DataBase):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    firstname = Column(String(45))
-    surname = Column(String(45))
-    patronimyc = Column(String(45))
+    username = Column(String(45))
+    password = Column(String(45))
+    fullname = Column(String(135))
     phone = Column(String(12))
     email = Column(String(45))
-    password = Column(String(45))
-    username = Column(String(45))
     client_id = Column(Integer, ForeignKey("client.id"))
     client = relationship("Client", backref=backref("user", uselist=False, cascade="all,delete"))
     walker_id = Column(Integer, ForeignKey("walker.id"))
@@ -29,9 +27,5 @@ class Client(DataBase):
 class Walker(DataBase):
     __tablename__ = 'walker'
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-# <<<<<<< Updated upstream
     rating = Column(Float(precision=10), default=0)
-# =======
-#     rating = Column(Float(precision=10))
-# >>>>>>> Stashed changes
     counter = Column(Integer)
