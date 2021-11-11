6a7580c6544feb1a964a6abd69c2e26eb976f71b
Metaraddin <48632825+Metaraddin@users.noreply.github.com>
2021-11-12 00:16:10 +0300

    avatar

--- src/schemas/dog.py
+++ src/schemas/dog.py
@@ -5,3 +5,4 @@ from typing import Optional
 class Dog(BaseModel):
     breed: str
     nickname: str
+    avatar_url: str
--- src/schemas/user.py
+++ src/schemas/user.py
@@ -14,6 +14,7 @@ class UserBase(BaseModel):
     fullname: str
     phone: str
     email: str
+    avatar_url: str
 
 
 class TokenBase(BaseModel):
